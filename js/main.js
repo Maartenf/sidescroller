c47b832023a4fbdf0931cd6458a1eee00bc0a2c9
Addisonbean <addisonbean@gmail.com>
2013-04-08 18:53:52 -0500

    Added requestAnimationFrame in main.js and made render and update functions

--- js/main.js
+++ js/main.js
@@ -6,9 +6,32 @@ $(document).ready(function() {
 	    ctx = canvas.getContext('2d'),
 	    isRunning;
 
+	// Better for animating then SetTimeout()
+	// Some browser lack support for it so this kinda fixes it in a way
+	window.requestAnimFrame = (function(){
+	 	return  window.requestAnimationFrame       ||
+        	window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+        	};
+	})();
+
+	// Updates the position of the stuff or whatever its gonna do
+	function update() {
+
+	}
+
+	// Renders the new scene
+	function render() {
+		
+	}
+
 	function gameLoop(running) {
 		while (running) {
-
+			requestAnimFrame(gameLoop);
+			update();
+			render();
 		}
 	}	
 
