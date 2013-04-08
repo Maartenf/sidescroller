fe221f63c94994d3b7b4097d5b92a87407979193
Maartenf <maartenforrier@hotmail.com>
2013-04-08 12:36:44 +0300

    Added basic functions and the call to a future class: GameLoop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 // This is the game's main JavaScript file
+
+// Globals
+var gameLoop;
+
 $(document).ready(function() {
+	
 	canvas = $('#canvas')[0];
 	ctx = canvas.getContext('2d');
+	
+	function startGame() {
+		
+		// Creates an instance of class GameLoop
+		var gameLoop = new GameLoop();
+		gameLoop.start();
+	}
+	
+	function stopGame() {
+		
+		// Stops the game loop e.g. for pause or exit
+		gameLoop.stop();
+	}
 });
